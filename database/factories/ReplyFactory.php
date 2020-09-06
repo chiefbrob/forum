1173706eada5ddee4bb4f4e74d244eda39f66795
Brian Chief Obare <brian.obare@pfizer.com>
2020-09-06 07:58:25 +0300

    setup [Thread, Reply] database, controller & factory

---
+++ database/factories/ReplyFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Reply;
+use App\Thread;
+use App\User;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(Reply::class, function (Faker $faker) {
+    return [
+        'user_id' => factory(User::class)->create(),
+        'thread_id' => factory(Thread::class)->create(),
+        'body' => $faker->paragraph
+    ];
+});
---
+++ database/factories/ThreadFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Thread;
+use App\User;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(Thread::class, function (Faker $faker) {
+    return [
+        'user_id' => factory(User::class)->create(),
+        'title' => $faker->sentence,
+        'body' => $faker->paragraph
+    ];
+});
